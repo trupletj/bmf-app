8de1d2e4cb0538f5bcbd1c4aa9fc662e4d515f09
trupletj <tuvshinjargal01@gmail.com>
2024-09-24 13:55:48 +0800

    sss

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
+import Footer from "@/components/Footer";
 
 
 export default function Home() {
@@ -11,6 +12,8 @@ export default function Home() {
       <Button>
         ЗАХИАЛГА ӨГӨХ
       </Button>
+      <div className="h-[600px]"></div>
+      <Footer />
     </div>
   );
 }
