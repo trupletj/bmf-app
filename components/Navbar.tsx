ae5b4005b3ede4003ae9edbe23f9ea4c02bc36f3
trupletj <tuvshinjargal01@gmail.com>
2024-09-24 13:47:21 +0800

    init

---
+++ components/Navbar.tsx
@@ -0,0 +1,144 @@
+import Link from "next/link"
+import Image from "next/image"
+import { CircleUser, Menu, Package2, Search } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Checkbox } from "@/components/ui/checkbox"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+
+import LogoSvg from "../app/logo.svg"
+
+const Navbar = () => {
+    return (
+        <div >
+            <header className="flex flex-wrap min-h-32 items-center justify-between  gap-4  px-4 md:px-6 relative z-50">
+                <div className="absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2">
+                    <Image
+                        width={113}
+                        height={61}
+                        alt="My Image Svg"
+
+                        src={LogoSvg} // Method 1
+
+                    // OR
+
+                    />
+                </div>
+                <nav className="hidden flex-col gap-6 text-md  md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+
+                    <Link
+                        href="#"
+                        className="text-muted-foreground transition-colors hover:text-foreground"
+                    >
+                        БИДНИЙ ТУХАЙ
+                    </Link>
+                    <Link
+                        href="#"
+                        className="text-muted-foreground transition-colors hover:text-foreground"
+                    >
+                        МЭДЭЭ МЭДЭЭЛЭЛ
+                    </Link>
+                    <Link
+                        href="#"
+                        className="text-muted-foreground transition-colors hover:text-foreground"
+                    >
+                        ОНЛАЙН ЗАХИАЛГА
+                    </Link>
+                    <Link
+                        href="#"
+                        className="text-muted-foreground transition-colors hover:text-foreground"
+                    >
+                        ХАМТРАН АЖИЛЛАХ
+                    </Link>
+
+                </nav>
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button
+                            variant="ghost"
+                            className="shrink-0 md:hidden"
+                        >
+                            <Menu className="h-8 w-8 text-primary" />
+                            <span className="sr-only">Toggle navigation menu</span>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="left">
+                        <nav className="grid gap-6 text-lg font-medium">
+                            <Link
+                                href="#"
+                                className="flex items-center gap-2 text-lg font-semibold"
+                            >
+                                <Package2 className="h-6 w-6" />
+                                <span className="sr-only">Acme Inc</span>
+                            </Link>
+                            <Link
+                                href="#"
+                                className="text-muted-foreground hover:text-foreground"
+                            >
+                                Dashboard
+                            </Link>
+                            <Link
+                                href="#"
+                                className="text-muted-foreground hover:text-foreground"
+                            >
+                                Orders
+                            </Link>
+                            <Link
+                                href="#"
+                                className="text-muted-foreground hover:text-foreground"
+                            >
+                                Products
+                            </Link>
+                            <Link
+                                href="#"
+                                className="text-muted-foreground hover:text-foreground"
+                            >
+                                Customers
+                            </Link>
+                            <Link href="#" className="hover:text-foreground">
+                                Settings
+                            </Link>
+                        </nav>
+                    </SheetContent>
+                </Sheet>
+                <div className="flex  items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="secondary" size="icon" className="rounded-full">
+                                <CircleUser className="h-5 w-5" />
+                                <span className="sr-only">Toggle user menu</span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Settings</DropdownMenuItem>
+                            <DropdownMenuItem>Support</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem><Button>button</Button></DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar
